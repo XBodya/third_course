4d8bb7deca1d665a80787d1a28c47a0e1e85ebd6
XBodya <bogdangorowenko5@gmail.com>
2024-11-12 19:24:21 +0300

    java l2 upd

---
+++ JAVA/l2/l2id/src/main/java/l2/ExpressionCalc.java
@@ -0,0 +1,17 @@
+package l2;
+
+public class ExpressionCalc {
+    String curExpression;
+
+    ExpressionCalc(String Expression) {
+        curExpression = Expression;
+    }
+
+    public double calculate() {
+        return 0;
+    }
+
+    private void toPostfix() {
+
+    }
+};
