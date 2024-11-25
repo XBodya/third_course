5bd308b0ddfd2b30923a8d6e40eb46b902843178
XBodya <bogdangorowenko5@gmail.com>
2024-11-25 14:31:45 +0300

    add testing

---
+++ JAVA/l2/l2id/target/test-classes/ExpressionCalcTest.java
@@ -0,0 +1,16 @@
+import static org.junit.Assert.assertTrue;
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Disabled;
+import org.junit.jupiter.api.Tag;
+import org.junit.jupiter.api.Test;
+
+public class ExpressionCalcTest {
+    @Test
+    public void test(){
+        Assertions.assertTrue(true, true);
+    }
+}
